c034cb5dd11df8e2594473c77537a725bf20ea22
huxiaofeng <huxiaofeng@agree.com.cn>
2019-03-04 13:48:16 +0800

    初始工程

---
+++ mall-manage/src/main/java/com/hxf/mall/Application.java
@@ -0,0 +1,14 @@
+package com.hxf.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hxf.mall.dao")
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
