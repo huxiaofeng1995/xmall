75fed382ff3c5da0ba899df31cb62a02abed3ef4
huxiaofeng <huxiaofeng@agree.com.cn>
2019-03-06 10:07:33 +0800

    实现商品图片上传的另一种方式

--- mall-manage/src/main/java/com/hxf/mall/mapper/SpuMapper.java
+++ mall-manage/src/main/java/com/hxf/mall/mapper/SpuMapper.java
@@ -3,8 +3,12 @@ package com.hxf.mall.mapper;
 import com.hxf.mall.bean.T_MALL_PRODUCT;
 import com.hxf.mall.bean.T_MALL_PRODUCT_IMAGE;
 
+import java.util.Map;
+
 public interface SpuMapper {
     void insert_spu(T_MALL_PRODUCT spu);
 
     void insert_spu_image(T_MALL_PRODUCT_IMAGE productImage);
+
+    void insert_imgs(Map<String, Object> map);
 }
--- mall-manage/src/main/java/com/hxf/mall/service/SpuService.java
+++ mall-manage/src/main/java/com/hxf/mall/service/SpuService.java
@@ -6,7 +6,9 @@ import com.hxf.mall.mapper.SpuMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SpuService {
@@ -18,12 +20,21 @@ public class SpuService {
         String img = imgs.get(0);//取第一张图片作为主图片
         spu.setShp_tp(img);
         spuMapper.insert_spu(spu);
-        for(String image : imgs){
-            T_MALL_PRODUCT_IMAGE productImage = new T_MALL_PRODUCT_IMAGE();
-            productImage.setShp_id(spu.getId());
-            productImage.setUrl(img);
-            spuMapper.insert_spu_image(productImage);
-        }
+//        方式1
+//        for(String image : imgs){
+//            T_MALL_PRODUCT_IMAGE productImage = new T_MALL_PRODUCT_IMAGE();
+//            productImage.setShp_id(spu.getId());
+//            productImage.setUrl(img);
+//            spuMapper.insert_spu_image(productImage);
+//        }
 
+        //方式2
+        //根据主键，批量插入spu图片
+        Map<String, Object> map = new HashMap<>();
+        map.put("shp_id", spu.getId());
+        map.put("imgs", imgs);
+        //spuMapper.insert_imgs(spu.getId(), imgs);
+        //遇到多参数传递时，最好封装成map，这样方便在mapper.xml中获取
+        spuMapper.insert_imgs(map);
     }
 }
