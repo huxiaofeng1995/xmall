0126ebb6aed6a2c6130e71f00c5cac328fecc7c2
huxiaofeng <huxiaofeng@agree.com.cn>
2019-03-13 15:35:18 +0800

    模拟用户登录和获取用户信息的接口，与后台UI初始项目调通

---
+++ mall-manage/src/main/java/com/hxf/mall/ctrl/UserController.java
@@ -0,0 +1,54 @@
+package com.hxf.mall.ctrl;
+
+import com.alibaba.fastjson.JSON;
+import com.hxf.mall.to.AMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @PostMapping("admin/login")
+    private String login(@RequestBody String username){
+
+        //省略数据库查询操作，待后期完善
+
+
+        AMessage aMessage = new AMessage();
+        aMessage.setCode(200);
+        Map<String,Object> data = new HashMap<>();
+        data.put("tokenHead","test");
+        data.put("token",System.currentTimeMillis());
+        aMessage.setData(data);
+        return JSON.toJSONString(aMessage);
+    }
+
+    @GetMapping("admin/info")
+    private String getUserInfo(HttpServletRequest request){
+        //获取请求token，验证该token用户是否登录
+        String token = request.getHeader("Authorization");
+
+        //省略数据库查询操作，待后期完善
+
+
+        AMessage aMessage = new AMessage();
+        aMessage.setCode(200);
+        Map<String,Object> data = new HashMap<>();
+        data.put("username","胡晓峰");
+        List<String>  roles = new ArrayList<>();
+        roles.add("admin");
+        data.put("roles",roles);
+        data.put("icon","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        aMessage.setData(data);
+        return JSON.toJSONString(aMessage);
+    }
+
+}
